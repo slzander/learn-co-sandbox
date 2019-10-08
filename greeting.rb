964844031758a3aace1aca8330fd63bdb0d281fb
Stacey Zander <staceyzander@gmail.com>
2019-10-08 05:29:59 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,6 @@
+def greeting_programmer(name, language = "computer")
+  puts "Hello #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting_programmer("Steve", "ruby")
+greeting_programmer("Maria")
---
+++ learning-comments.rb
@@ -0,0 +1,7 @@
+magic_exit = 7
+count = 1
+while count < 10 && count != magic_exit do
+  puts "I am number #{count}"
+  count += 2
+end
+
